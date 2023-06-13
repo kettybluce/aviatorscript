da89925ac65cb046109f486c78d7a701ab39cd74
张立强 <ketty9527@163.com>
2023-06-13 20:04:23 +0800

    >4.结构重构

--- src/main/java/com/aviator/ketty/rule/AviatorExecutor.java
+++ src/main/java/com/aviator/ketty/rule/AviatorExecutor.java
@@ -3,7 +3,6 @@ package com.aviator.ketty.rule;
 import com.googlecode.aviator.AviatorEvaluator;
 import com.googlecode.aviator.AviatorEvaluatorInstance;
 import com.googlecode.aviator.EvalMode;
-import com.googlecode.aviator.Options;
 
 import java.util.Map;
 
@@ -17,7 +16,7 @@ public class AviatorExecutor {
         // 创建解释器
         AviatorEvaluatorInstance engine = AviatorEvaluator.newInstance(EvalMode.INTERPRETER);
         // 打开跟踪执行
-        engine.setOption(Options.TRACE_EVAL, true);
+//        engine.setOption(Options.TRACE_EVAL, true);
 
 //        Map<String, Object> param = AviatorContext.buildMap(context);
         T execute = (T) engine.execute(expression, context, true);
